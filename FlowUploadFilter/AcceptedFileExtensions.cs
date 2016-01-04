b6f1f87a760c0421c66db8fc682995a118290141
Stefan <stefan@dreambyte.eu>
2016-01-04 22:58:17 +0100

    Accept upload-request if the file extensions has a different case. Extract functionality into AcceptedFileExtensions-class.

---
+++ FlowUploadFilter/AcceptedFileExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FlowUploadFilter
+{
+    public class AcceptedFileExtensions
+    {
+        public bool IsExtensionAllowed(IList<string> allowedExtensions, string fileName)
+        {
+            var isAllowed = false;
+
+            // Extract extension from FileName ("Test.txt" => ".txt")
+            var fileExtension = Path.GetExtension(fileName);
+
+            // fileExtension will be empty if the file has no extension
+            if (!string.IsNullOrEmpty(fileExtension))
+            {
+                // GetExtension returns the extension with the leading-zero. The leading zero will be removed here for easier handling:
+                fileExtension = fileExtension.TrimStart('.');
+
+                // Search the allowedExtension-List for the file extension (case is ignored)
+                // Test.TXT is accepted as well as Test.txt even if only .txt is in the list of allowed extensions
+                isAllowed = allowedExtensions.Contains(fileExtension, StringComparer.InvariantCultureIgnoreCase);
+            }
+
+            return isAllowed;
+        }
+    }
+}
