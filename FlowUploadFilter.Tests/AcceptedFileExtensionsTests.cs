253af2e46543ff4f2d1b6d3d460d2c41b203eae3
Stefan <stefan@dreambyte.eu>
2016-01-04 22:58:45 +0100

    Introduce unit-tests for the project. Added tests for "AcceptedFileExtensions"-class.

---
+++ FlowUploadFilter.Tests/AcceptedFileExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlowUploadFilter.Tests
+{
+    [TestClass]
+    public class AcceptedFileExtensionTests
+    {
+        [TestMethod]
+        public void ExtensionAccepted_SameCase()
+        {
+            var tester = new AcceptedFileExtensions();
+
+            var acceptedExtensions = new List<string> {"txt", "png", "pdf"};
+
+            var result = tester.IsExtensionAllowed(acceptedExtensions, "test.txt");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ExtensionAccepted_DifferentCase()
+        {
+            var tester = new AcceptedFileExtensions();
+
+            var acceptedExtensions = new List<string> { "txt", "png", "pdf" };
+
+            var result = tester.IsExtensionAllowed(acceptedExtensions, "test.TXT");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ExtensionInvalid_InvalidExtension()
+        {
+            var tester = new AcceptedFileExtensions();
+
+            var acceptedExtensions = new List<string> { "txt", "png", "pdf" };
+
+            var result = tester.IsExtensionAllowed(acceptedExtensions, "test.invalid");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ExtensionInvalid_NoExtension()
+        {
+            var tester = new AcceptedFileExtensions();
+
+            var acceptedExtensions = new List<string> { "txt", "png", "pdf" };
+
+            var result = tester.IsExtensionAllowed(acceptedExtensions, "test");
+
+            Assert.IsFalse(result);
+        }
+    }
+}
